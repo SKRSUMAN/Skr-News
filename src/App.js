b83065227060deda6a37e8fd90935d73502eea62
SUMAN KUMAR <113022724+SKRSUMAN@users.noreply.github.com>
2025-08-09 01:13:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With main
+          Learn React With Master
         </a>
       </header>
     </div>
