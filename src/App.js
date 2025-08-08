0af2f7ae74066bada3f2dd13f015329720b3fed9
SUMAN KUMAR <113022724+SKRSUMAN@users.noreply.github.com>
2025-08-09 00:41:21 +0530

    suman.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Suman.
+          Edit <code>src/App.js</code> and save to reload Sumank.
         </p>
         <a
           className="App-link"
